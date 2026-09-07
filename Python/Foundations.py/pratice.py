2421a715941be5db76b64604c0cde2c41c31ba21
Rajkumar200409 <rajkumar20040909@gmail.com>
2026-09-07 17:25:50 +0530

    Learn variables, data types

---
+++ Python/Foundations.py/pratice.py
@@ -0,0 +1,3 @@
+print('"hellow world"')
+print('"Rajkumar"')
+print(8%4,8/4,8/4)
---
+++ Python/Foundations.py/preatice2.py
@@ -0,0 +1,13 @@
+print("student name is :Arun")
+print("student age is :20")
+print("student degree is : computer science")
+
+print("name :Arun \nAge :20 \nDegree : computer science")
+
+Name = "Arun"
+Age =20
+print(Name)
+name2 = input("Enter your name:")
+print(f"The name of Second Student is: {name2}, And the age of first student is: {Age}")
+
+
